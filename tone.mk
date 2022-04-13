5b77deb9f9c9e99a2e005ab41ca2d35db4fff8d7
Arian <arian.kulmer@web.de>
2022-04-14 00:12:50 +0200

    tone-common: Don't explicitly build fs_config_files
    The fs_config_files for each partition is already added to
    PRODUCT_PACKAGES by base_{product,system,system_ext,vendor}.mk.
    
    Change-Id: Ifbadacd5cb20beca70ed8b3accb3baf86a989c92

--- tone.mk
+++ tone.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.1-service.sony
 
-# For config.fs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
