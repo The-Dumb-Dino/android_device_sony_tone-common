ccf3201c83b2bf5fb9db7acfe16e82ff03ccdeed
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    tone-common: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- tone.mk
+++ tone.mk
@@ -413,7 +413,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libpower.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
